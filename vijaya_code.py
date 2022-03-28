ee6bb6d5bf567d21b9643bb5648e61327dc1a2e0
vijaya pardha saradhi tummala <35499492+vijaytummala-pythondeveloper@users.noreply.github.com>
2022-03-28 13:01:36 +0530

    Update vijaya_code.py

--- vijaya_code.py
+++ vijaya_code.py
@@ -1,3 +1,4 @@
+
 import faker as fake
 import datetime
 import time
@@ -67,8 +68,7 @@ while True:
         count = count + 1
         data = data + generate()+'\n'
     filename = f'uber_data_{time.time()}_{count+1}.log'
-    file = open('C:\\Users\\rakray\\Documents\\Deloitte_Training\\case_study_2\\code\\deloitte-case-study-2\\logs\\{}'.format(filename),'w')
+    file = open('C:\\sample\\{}'.format(filename),'w')
     file.write(data)
     file.close()
     time.sleep(10)
-    
