084682d5f75ce2be31ddf56c65f436cd7f727729
Raktim Ray <raktimray.india@gmail.com>
2022-03-28 16:10:37 +0530

    updates

--- group_3_code.py
+++ group_3_code.py
@@ -16,38 +16,42 @@ def main():
         for i in add:
             address += i + ' '
         return address
-    def compare_times(time1,time2):
-        if int(time1.split(':')[0]) + 5 > int(time2.split(':')[0]):
-            time11 = datetime.datetime.strptime(time1, '%H:%M:%S')
-            time22 = datetime.datetime.strptime(time2, '%H:%M:%S')
-            return time22 > time11
-        else:
-            return False
+
+    # book and pick 5 to 15 min
+    def pick_gen(book_time):
+        min_list = list(range(5,16))
+        min = random.choice(min_list)
+        # b = datetime.datetime.strptime(book_time, '%H:%M:%S')
+        return (book_time  + datetime.timedelta(minutes=min))
+    # pick  and drop 15 to 120 min
+    def drop_gen(pick_time):
+        min_list = list(range(15,120))
+        min = random.choice(min_list)
+        # b = datetime.datetime.strptime(pick_time, '%H:%M:%S')
+        return (pick_time  + datetime.timedelta(minutes=min))
+    # check address
     def addres_check(pick,drop):
         if pick != drop:
             return True
 
+    # price cal
+    def price(min):
+        res  = 3 * min + random.randint(1,55)
+        return res
 
 
-    customer_id_list = [i for i in range(101,1001)]
+
+    customer_id_list = [i for i in range(100,1001)]
     def generate():
         customer_id = random.choice(customer_id_list)
-        booking_date = f.date()
-        booking_time = f.time()
+        booking_date = datetime.datetime.now().date()
+        booking_time = datetime.datetime.now().time()
+        book_date_time = datetime.datetime.now()
+        pick_date_time = pick_gen(book_date_time)
+        pick_time = pick_date_time.time()
+        drop_date_time = drop_gen(pick_date_time)
+        drop_time = drop_date_time.time()
         pick_up_address = address_gen()
-        pick_flag = True
-        pick = ''
-        while pick_flag:
-            pick = f.time()
-            if compare_times(booking_time,pick):
-                pick_flag = False
-        
-        drop_flag = True
-        drop = ''
-        while drop_flag:
-            drop = f.time()
-            if compare_times(pick,drop):
-                drop_flag = False
         drop_address = address_gen()
         add_flag = True
         while add_flag:
@@ -57,23 +61,22 @@ def main():
         if customer_id in gender_dict.keys():
             gender = gender_dict[customer_id]
         else:
-            gender = (random.choice(["male", "female", "other"]))
+            gender = (random.choice(["male", "female"]))
             gender_dict[customer_id] = gender
-        return f'{customer_id};{gender};{booking_date};{booking_time};{pick_up_address};{pick};{drop};{drop_address}'
+        minutes_of_ride = (drop_date_time - pick_date_time).total_seconds() /60
+        price_val = price(minutes_of_ride)
+        
+        return f'{customer_id};{gender};{booking_date};{booking_time};{pick_up_address};{pick_time};{drop_time};{drop_address};{price_val}'
 
     while True:
         no = random.randint(20,101)
-        count = 0
         data = ''
         for i in range(1,no+1):
-            count = count + 1
             data = data + generate()+'\n'
-        timestr = time.strftime("%d_%m_%Y_%H_%M_%S")
-        filename = f'uber_data_{timestr}.log'
+        filename = f"uber_data_{(datetime.datetime.now().date())}_{(datetime.datetime.now().hour)}_{(datetime.datetime.now().minute)}_{(datetime.datetime.now().second)}.log"
         file = open('C:\\Users\\rakray\\Documents\\Deloitte_Training\\case_study_2\\code\\deloitte-case-study-2\\logs\\{}'.format(filename),'w')
         file.write(data)
         file.close()
-        print("log created")
         time.sleep(10)
 
 if __name__ == "__main__":
